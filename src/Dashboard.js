cacf273a29b880d0989097a93b8c5e9b8d8db60a
Dorotka <dorotamamczak@gmail.com>
2019-05-27 11:42:56 +0200

    created spa

---
+++ src/Dashboard.js
@@ -0,0 +1,246 @@
+import React from 'react';
+import './Dashboard.css';
+import { withRouter } from 'react-router-dom';
+
+
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuText: "",
+            submenuText: "",
+            notifications: ['Error', 'Warning', 'Info'],
+            colorOfNotifications: ['red', 'orange', 'blue'],
+            date: new Date().toLocaleString(),
+            displayNotification: '',
+            selectedColorOfNotifications: '',
+        };
+        let targetDispalyText = '';
+    }
+
+    handleClickBackToLogin = (e) => {
+        e.preventDefault();
+        let path = `/`;
+        this.props.history.push(path);
+    };
+
+    componentDidMount = () => {
+        const randomTime = Math.floor(Math.random() * (10 - 5 + 1) + 5) * 1000;
+        this.timeout = setTimeout(() => {
+            console.log(randomTime)
+            const randomIndexOfNotification = Math.floor(Math.random() * this.state.notifications.length);
+            const displayedNotification = `${this.state.notifications[randomIndexOfNotification]} - date: ${this.state.date}`;
+            this.setState({
+                displayNotification: displayedNotification,
+                date: new Date().toLocaleString(),
+                selectedColorOfNotifications: this.state.colorOfNotifications[randomIndexOfNotification],
+            });
+            this.componentDidMount();
+        }, randomTime);
+    };
+
+    componentWillUnmount = () => {
+        clearTimeout(this.timeout);
+    };
+
+    dismiss = () => {
+        clearTimeout(this.timeout);
+        this.setState({
+            displayNotification: '',
+            date: '',
+            selectedColorOfNotifications: '',
+        });
+    };
+
+    render() {
+
+        const handleClickDispalyText = (e) => {
+            if (e.target.classList.contains('menu')) {
+                this.setState({
+                    menuText: this.targetDispalyText,
+                    submenuText: e.target.textContent,
+                })
+                this.targetDispalyText = e.target.textContent;
+            }
+            this.setState({
+                menuText: this.targetDispalyText,
+                submenuText: e.target.textContent,
+            })
+        };
+
+        let offset = [0, 0];
+        let divOverlay = document.getElementById("accordionWindow");
+        let isDown = false;
+        divOverlay.addEventListener('mousedown', function (e) {
+            isDown = true;
+            offset = [
+                divOverlay.offsetLeft - e.clientX,
+                divOverlay.offsetTop - e.clientY
+            ];
+        }, true);
+        document.addEventListener('mouseup', function () {
+            isDown = false;
+        }, true);
+
+        document.addEventListener('mousemove', function (e) {
+            e.preventDefault();
+            if (isDown) {
+                divOverlay.style.left = (e.clientX + offset[0]) + 'px';
+                divOverlay.style.top = (e.clientY + offset[1]) + 'px';
+            }
+        }, true);
+
+        return (
+            <div className="wrapper" >
+                <nav id="sidebar">
+                    <div className="sidebar-header">
+                        <h3>Menu</h3>
+                    </div>
+                    <ul className="list-unstyled components">
+                        <li>
+                            <a className='menu' onClick={handleClickDispalyText} href="#">Dashboard</a>
+                        </li>
+                        <li>
+                            <a onClick={handleClickDispalyText}
+                                href="#dashboard"
+                                data-toggle="collapse"
+                                aria-expanded="false"
+                                className="menu dropdown-toggle">Statistics</a>
+                            <ul className="collapse list-unstyled" id="dashboard">
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Tests</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Devices</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Builds</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Services</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Projects</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a onClick={handleClickDispalyText}
+                                href="#reports"
+                                data-toggle="collapse"
+                                aria-expanded="false"
+                                className="menu dropdown-toggle">Reports</a>
+                            <ul className="collapse list-unstyled" id="reports">
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Tests</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Devices</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Builds</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Services</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Projects</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a className="menu"
+                                onClick={handleClickDispalyText}
+                                href="#">Configurations</a>
+                        </li>
+                        <li>
+                            <a onClick={handleClickDispalyText}
+                                href="#configurations"
+                                data-toggle="collapse"
+                                aria-expanded="false" className="menu dropdown-toggle">Overview</a>
+                            <ul className="collapse list-unstyled" id="configurations">
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Tests</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Devices</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Builds</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Services</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleClickDispalyText}
+                                        href="#">Projects</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div id="header">
+                    <p className='notifications' style={{ color: this.state.selectedColorOfNotifications }}>{this.state.displayNotification}</p>
+                    {this.state.displayNotification ? <button
+                        id='dismiss'
+                        className="btn btn-primary dashboard"
+                        type="submit"
+                        onClick={this.dismiss}
+                    >Dismiss</button> : null}
+                    <button
+                        id='backToLogin'
+                        className="btn btn-primary dashboard"
+                        type="submit"
+                        onClick={this.handleClickBackToLogin}
+                    >Logout</button>
+                    <button
+                        id='displayConfigurationPage'
+                        className="btn btn-primary dashboard"
+                        type="submit"
+                        onClick={this.handleClickDisplayConfigurationPage}
+                    >Configuration</button>
+                </div>
+
+                <div id="main-content">
+                    <p className='display-menu'>{`${this.state.menuText} ${this.state.submenuText === this.state.menuText ? '' : '- ' + this.state.submenuText}`}</p>
+
+                    <div className="accordion" id="accordionWindow">
+                        <div className="card">
+                            <div className="card-header" id="heading">
+                                <h5 className="mb-0">
+                                    <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse" aria-expanded="true" aria-controls="collapse">
+                                        Collapsible window
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapse" className="collapse show" aria-labelledby="heading" data-parent="#accordionWindow">
+                                <div className="card-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rem modi ea mollitia numquam.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div >
+        );
+    }
+}
+
+
+
+export default withRouter(Dashboard);
---
+++ src/Login.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import './Login.css';
+// import { Link, NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+
+
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputLoginValue: '',
+            inputPasswordValue: '',
+        };
+    }
+
+    handleInputLogin = (e) => {
+        const inputLoginValue = e.target.value.trim();
+        this.setState({
+            inputLoginValue: inputLoginValue,
+        });
+    };
+
+    handleInputPassword = (e) => {
+        const inputPasswordValue = e.target.value.trim();
+        this.setState({
+            inputPasswordValue: inputPasswordValue,
+        });
+    };
+
+    handleClickGoToDashboard = (e) => {
+        e.preventDefault();
+        let path = `/Dashboard`;
+        this.props.history.push(path);
+    };
+
+    render() {
+        return (
+            <div className="wrap">
+                <div className="login" >
+                    <form className="form-signin">
+                        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                        <label
+                            htmlFor="inputLogin"
+                            className="sr-only">Login</label>
+                        <input type="text"
+                            id="inputLogin"
+                            className="form-control"
+                            placeholder="Login"
+                            required
+                            autoFocus
+                            value={this.state.inputLoginValue}
+                            onChange={this.handleInputLogin}
+                        ></input>
+                        <label
+                            htmlFor="inputPassword"
+                            className="sr-only">Password</label>
+                        <input
+                            type="password"
+                            id="inputPassword"
+                            className="form-control"
+                            placeholder="Password"
+                            required
+                            value={this.state.inputPasswordValue}
+                            onChange={this.handleInputPassword}
+                        ></input>
+                        <button
+                            id='btnLogin'
+                            className="btn btn-lg btn-primary"
+                            type="submit"
+                            disabled={!(this.state.inputLoginValue && this.state.inputPasswordValue)}
+                            onClick={this.handleClickGoToDashboard}
+                        >Sign in</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withRouter(Login);
